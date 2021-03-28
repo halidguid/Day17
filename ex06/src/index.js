0f121c448274d25ae3e816adea0128a9c623de76
halidguid <halidhosi@gmail.com>
2021-03-28 18:21:24 +0200

    idetop

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,14 +1,13 @@
 const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function removeFirstTwo(list) {
-    "use strict";
-    // Only change code below this line
+  "use strict";
+  // Only change code below this line
 
-    const [a, b, ...arr] = list;
-    const array2 = arr; // Change this line
+  const [, , ...array2] = list; // Change this line
 
-    // Only change code above this line
-    return array2;
+  // Only change code above this line
+  return array2;
 }
 
 console.log(removeFirstTwo(array1));
